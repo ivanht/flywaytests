a7330a1dd1eac75ec12188b95691fe0492fabb4f
Ivan Hitos <ivanhitos@gmail.com>
2018-07-20 11:13:55 +0000

    fifth commit

---
+++ V1_7__nonrecovery_error.sql
@@ -0,0 +1,2 @@
+TRUNCATE `myothertable`;
+INSERT INTO `myothertable2` VALUES (4,'laptop2','1000'),(5,'mouse2','100'),(6,'keyboard2','200');
---
+++ V1_8__recoverable_error.sql
@@ -0,0 +1,2 @@
+DELETE FROM `myothertable` WHERE id<10;
+INSERT INTO `myothertable` VALUES (44,'laptop2','1000',11),(45,'mouse2','100'),(46,'keyboard2','200');
